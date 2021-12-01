c18e3037cecf9d1436fa13ff2d045dafdcb467c6
Anthony-Minecraft <75374143+Anthony-Minecraft@users.noreply.github.com>
2021-11-30 21:30:33 -0500

    Add files via upload

---
+++ Java V3-Gui/Main.java
@@ -0,0 +1,168 @@
+/*
+ * Copyright (c) 1995, 2008, Oracle and/or its affiliates. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ *   - Neither the name of Oracle or the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+ 
+import java.io.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.SwingUtilities;
+import javax.swing.filechooser.*;
+ 
+/*
+ * FileChooserDemo.java uses these files:
+ *   images/Open16.gif
+ *   images/Save16.gif
+ */
+public class Main extends JPanel
+                             implements ActionListener {
+    static private final String newline = "\n";
+    JButton openButton, saveButton, listButton;
+    JTextArea log;
+    JFileChooser fc;
+ 
+    public Main() {
+        super(new BorderLayout());
+ 
+        //Create the log first, because the action listeners
+        //need to refer to it.
+        log = new JTextArea(5,20);
+        log.setMargin(new Insets(5,5,5,5));
+        log.setEditable(false);
+        JScrollPane logScrollPane = new JScrollPane(log);
+ 
+
+        fc = new JFileChooser();
+        fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        
+        openButton = new JButton("Add mark");
+        openButton.addActionListener(this);
+ 
+        saveButton = new JButton("Remove mark");
+        saveButton.addActionListener(this);
+        
+        listButton = new JButton("List a directory");
+        listButton.addActionListener(this);
+ 
+
+        JPanel buttonPanel = new JPanel(); //use FlowLayout
+        buttonPanel.add(openButton);
+        buttonPanel.add(saveButton);
+        buttonPanel.add(listButton);
+ 
+
+        add(buttonPanel, BorderLayout.PAGE_START);
+        add(logScrollPane, BorderLayout.CENTER);
+    }
+ 
+    public void actionPerformed(ActionEvent e) {
+ 
+        //Handle open button action.
+        if (e.getSource() == openButton) {
+            int returnVal = fc.showOpenDialog(Main.this);
+ 
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                File file = fc.getSelectedFile();
+                //This is where a real application would open the file.
+                log.append("Adding marks to: " + file.getName() + "." + newline);
+                programlogic b = new programlogic();
+                b.programLogic(file.getAbsolutePath());
+            } else {
+                log.append("Cancelled" + newline);
+            }
+            log.setCaretPosition(log.getDocument().getLength());
+ 
+        //Handle save button action.
+        } else if (e.getSource() == saveButton) {
+            int returnVal = fc.showSaveDialog(Main.this);
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                File file = fc.getSelectedFile();
+                //This is where a real application would save the file.
+                removemark a = new removemark();
+                a.removeMark(file.getAbsolutePath());
+                log.append("Removing marks from: " + file.getName() + "." + newline);
+            } else {
+                log.append("Cancelled." + newline);
+            }
+            log.setCaretPosition(log.getDocument().getLength());
+        } else if (e.getSource() == listButton) {
+            int returnVal = fc.showSaveDialog(Main.this);
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                File file = fc.getSelectedFile();
+                //This is where a real application would save the file.
+                listBash(file.getAbsolutePath());
+            } else {
+                log.append("Cancelled." + newline);
+            }
+            log.setCaretPosition(log.getDocument().getLength());
+    }
+    } 
+    
+ 
+    /**
+     * Create the GUI and show it.  For thread safety,
+     * this method should be invoked from the
+     * event dispatch thread.
+     */
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("File Sequence Randomizer (RanPre)");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ 
+        //Add content to the window.
+        frame.add(new Main());
+ 
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+    
+    public void listBash(String folder) {
+        String[] lspathnames;
+        File f = new File(folder);
+        lspathnames = f.list();
+        for (String lspathname : lspathnames) {
+        	log.append(lspathname + newline);
+        }
+      }
+ 
+    public static void main(String[] args) {
+        //Schedule a job for the event dispatch thread:
+        //creating and showing this application's GUI.
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                //Turn off metal's use of bold fonts
+                UIManager.put("swing.boldMetal", Boolean.FALSE); 
+                createAndShowGUI();
+            }
+        });
+    }
+}
